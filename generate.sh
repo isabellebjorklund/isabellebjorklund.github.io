da70ee60592cf0c9c39f207a70893cb86e5fb85c
Peter Bjorklund <piot@hotmail.com>
2016-02-11 16:51:30 -0500

    Added linkedin

--- generate.sh
+++ generate.sh
@@ -1,9 +1,11 @@
+rm -rf ../output
 ruby ../../work/remark/src/main.rb $@
-rm -rf  portfolio
-rm -rf about
-cp -r ../output/portfolio .
-cp -r ../output/about .
+rm -rf portfolio/
+rm -rf about/
+cp -r ../output/portfolio/ .
+cp -r ../output/about/ .
 cp ../output/base.css .
 cp ../output/index.html .
 cp ../output/hero.jpg .
+cp ../output/linkedin.svg .
 
